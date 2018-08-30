0b0f988febec8f1b42e5ded44319a3cd36016018
hongduclamvt@gmail.com <bitbucket!@#$>
2018-08-30 16:50:28 +0700

    Init proxy server

---
+++ app.js
@@ -0,0 +1,34 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var proxy = require('express-http-proxy');
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/sanctions', function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, DELETE, PATCH');
+  return proxy('https://secure.underwriting-support.com:443/fst/api/fst_api.php').apply(null, arguments);
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
---
+++ bin/www.js
@@ -0,0 +1,28 @@
+var http = require('http'),
+  httpProxy = require('http-proxy');
+
+//
+// Create a proxy server with custom application logic
+//
+var proxy = httpProxy.createProxyServer({
+  secure: false
+});
+
+//
+// Create your custom server and just call `proxy.web()` to proxy
+// a web request to the target passed in the options
+// also you can use `proxy.ws()` to proxy a websockets request
+//
+var server = http.createServer(function (req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+  if (req.url.indexOf('sanctions-check') > 0) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, DELETE, PATCH');
+    proxy.web(req, res, {target: 'https://secure.underwriting-support.com:443/fst/api/fst_api.php'});
+  }
+});
+
+console.log("listening on port 5050")
+server.listen(5050);
